5b080ffeb7f7700f708e0eadaae71d8391a10071
eerojantunen <jant.eero@gmail.com>
2025-04-14 16:05:42 +0300

    update backend

--- part3/puhelinluettelo-backend/index.js
+++ part3/puhelinluettelo-backend/index.js
@@ -88,7 +88,7 @@ app.use(morgan(":method :url :status :res[content-length] :response-time ms :per
   })
 
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
